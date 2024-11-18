b4eadf62deb75690cd0178f3715bce6e6ac57756
Julian <julian.dobler@web.de>
2024-11-18 02:35:52 +0100

    Model fix and Fine Tuning Training NO Lazy Loading

--- replibert/replibert/training.py
+++ replibert/replibert/training.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import yaml
 from configuration.config import settings, get_logger
 from data.training.dataset import TrainingDataset
+from data.finetuning.datasets import FineTuningDataset, SST2Dataset
 from model import Bert, BertTuned
 from model_init import initialize_from_hf_model
 from transformers import BertTokenizer
