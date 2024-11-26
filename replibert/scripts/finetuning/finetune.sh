9e2f6fd8bf6efe51387df1249b76d8445de8bc2d
Yannik Queisler <dev.yannik.queisler@gmail.com>
2024-11-26 20:04:09 +0100

    Add script

--- replibert/scripts/finetuning/finetune.sh
+++ replibert/scripts/finetuning/finetune.sh
@@ -6,12 +6,12 @@
 # Check if the script is being executed or submitted
 if [ -z "$SLURM_JOB_ID" ]; then
     # Not running as an sbatch job, submit itself
-    sbatch --output=$HOME/pml-bert/replibert/logs/run.log "$0" "$@"
+    sbatch --output=$HOME/pml-bert/replibert/logs/finetune.log "$0" "$@"
     exit
 fi
 
 options="$@"
 
 echo 'Running replibert...'
-apptainer run --bind /home/space/datasets:/home/space/datasets pml.sif python replibert/main.py $options
+apptainer run --bind /home/space/datasets:/home/space/datasets pml.sif python replibert/main.py finetune $options
 echo 'Replibert execution completed.'
